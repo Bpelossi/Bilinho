724820cd69c1b00e786313061ea815ba173f3428
Bpelossi <brunopelossi@gmail.com>
2025-06-23 08:30:20 -0300

    fixing

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,15 +1,12 @@
 class Student < ApplicationRecord
   has_many :enrollments
 
-  enum gender: { male: "M", female: "F" }, _suffix: true
-  enum payment_method: { boleto: "Boleto", cartao: "Cartão" }
-
   validates :name, presence: true,
                    uniqueness: { case_sensitive: true }
   validates :cpf, presence: true,
                   uniqueness: true,
                   format: { with: /\A\d+\z/, message: "CPF deve conter apenas números" },
                   length: { is: 11 }
-  validates :gender, presence: true
-  validates :payment_method, presence: true
+  validates :gender, presence: true, inclusion: { in: %w[M F] }
+  validates :payment_method, presence: true, inclusion: { in: %w[boleto cartão] }
 end
