9eaa750901dbda7b530f4f2ab714b486b01746ca
Bpelossi <brunopelossi@gmail.com>
2025-06-25 14:55:34 -0300

    Adicionar criação e exibição do invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,22 +1,50 @@
 class InvoicesController < ApplicationController
   def index
+    @invoices = Invoice.all
+
+    if params[:id].present?
+      @invoices = @invoices.where("id LIKE ?", "%#{params[:id]}%")
+    end
+
+    if params[:invoice_price].present?
+      @invoices = @invoices.where("invoice_price LIKE ?", "%#{params[:invoice_price]}%")
+    end
+
+    if params[:invoice_due_date].present?
+      @invoices = @invoices.where("invoice_due_date LIKE ?", "%#{params[:invoice_due_date]}%")
+    end
+
+    if params[:status].present?
+      @invoices = @invoices.where(status: params[:status])
+    end
   end
+
 
   def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
+    @invoice = Invoice.find(params[:id])
   end
 
   def update
+    @invoice = Invoice.find(params[:id])
+    if @invoice.update(invoice_params)
+      redirect_to @invoice, notice: "Instituição atualizada com sucesso!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @invoice = Invoice.find(params[:id])
+    if @invoice.update(status: "disabled")
+      redirect_to @invoice, notice: "Instituição desabilitada com sucesso!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:invoice_price, :invoice_due_date, :status, :enrollment_id)
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,9 +3,5 @@ class Invoice < ApplicationRecord
 
   validates :invoice_price, presence: true
   validates :invoice_due_date, presence: true
-  validates :status, presence: true, inclusion: { in: %w[open late paid] }
-
-  after_initialize do
-    self.status ||= "open"
-  end
+  validates :status, presence: true, inclusion: { in: %w[open late paid disabled] }
 end
